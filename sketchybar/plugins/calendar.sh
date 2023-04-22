0b4cead805237d942f879a56e0b7aa742f7fe4d3
claude-perrin <vity5.diduk@gmail.com>
2023-04-22 20:35:48 +0200

    time is highligted red when it is 22h

--- sketchybar/plugins/calendar.sh
+++ sketchybar/plugins/calendar.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
+current_hour=$(date '+%H')
+if [ $current_hour -gt 21 ]; then
+    hour_color=0xffD22B2B
+    sketchybar --set $NAME icon="$(date '+%a %d. %b')" label="$(date '+%H:%M')" label.color=$hour_color
+fi
 sketchybar --set $NAME icon="$(date '+%a %d. %b')" label="$(date '+%H:%M')"
