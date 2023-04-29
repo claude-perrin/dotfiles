8749606dc4a9d9120d0ec1c09259fd9123090a45
claude-perrin <vity5.diduk@gmail.com>
2023-04-29 10:13:31 +0200

    nvim plugins + fixed calendar

--- sketchybar/plugins/calendar.sh
+++ sketchybar/plugins/calendar.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 current_hour=$(date '+%H')
 if [ $current_hour -gt 21 ]; then
-    hour_color=0xffD22B2B
-    sketchybar --set $NAME icon="$(date '+%a %d. %b')" label="$(date '+%H:%M')" label.color=$hour_color
+    hour_color=0xffD22B2B # Red
+else
+    hour_color=0xffcad3f5 # White
 fi
-sketchybar --set $NAME icon="$(date '+%a %d. %b')" label="$(date '+%H:%M')"
+sketchybar --set $NAME icon="$(date '+%a %d. %b')" label="$(date '+%H:%M')" label.color=$hour_color
